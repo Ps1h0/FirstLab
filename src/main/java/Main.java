bbc224586a7934db33e37a832d0027d77fdf2900
DvynilHub <belonogov220@gmail.com>
2021-02-01 22:52:57 +0800

    Add a comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import Reader.XMLReader;
+import UsHandler.UsHandler;
 import Writer.Writer;
 import org.xml.sax.SAXException;
 
@@ -8,10 +9,14 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        try {
 
-        XMLReader reader = new XMLReader();
-        reader.createDocument("src/main/resources/text.xml");
-        Writer writer = new Writer();
-        writer.writeToTxt(reader.getPOJO());
+            XMLReader reader = new XMLReader();
+            reader.createDocument("src/main/resources/txt.xml");
+            Writer writer = new Writer();
+            writer.writeToTxt(reader.getPOJO());
+        } catch (Exception e){
+            new UsHandler(e, "Main");
+        }
     }
 }
